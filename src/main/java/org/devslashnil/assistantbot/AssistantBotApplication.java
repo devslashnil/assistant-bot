83f2446326ddb4a0291f4c3e7ae4752de3f15742
Mikhail Potapov <devslashnil@gmail.com>
2022-08-04 22:10:38 +0300

    first commit

---
+++ src/main/java/org/devslashnil/assistantbot/AssistantBotApplication.java
@@ -0,0 +1,13 @@
+package org.devslashnil.assistantbot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AssistantBotApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AssistantBotApplication.class, args);
+	}
+
+}
---
+++ src/test/java/org/devslashnil/assistantbot/AssistantBotApplicationTests.java
@@ -0,0 +1,13 @@
+package org.devslashnil.assistantbot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AssistantBotApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
